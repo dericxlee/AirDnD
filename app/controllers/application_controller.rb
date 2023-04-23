c369c239660446b1fb49c3b97dce3cfd71d87fc9
Deric <dericleee@gmail.com>
2023-04-23 14:16:46 -0700

    csrf

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,10 @@
 class ApplicationController < ActionController::API
+    rescue_from StandardError, with: :unhandled_error
+    rescue_from ActionController::InvalidAuthenticityToken,
+        with: :invalid_authenticity_token
+    
     include ActionController::RequestForgeryProtection
-  
+    
     protect_from_forgery with: :exception
     before_action :snake_case_params, :attach_authenticity_token
 
@@ -43,6 +47,23 @@ class ApplicationController < ActionController::API
           render json: { user: current_user.slice('id', 'username', 'session_token') }
         else
           render json: ['No current user']
+        end
+    end
+
+    def invalid_authenticity_token
+        render json: { message: 'Invalid authenticity token' }, 
+            status: :unprocessable_entity
+    end
+      
+    def unhandled_error(error)
+        if request.accepts.first.html?
+            raise error
+        else
+            @message = "#{error.class} - #{error.message}"
+            @stack = Rails::BacktraceCleaner.new.clean(error.backtrace)
+            render 'api/errors/internal_server_error', status: :internal_server_error
+            
+            logger.error "\n#{@message}:\n\t#{@stack.join("\n\t")}\n"
         end
     end
 
