b9942e3f5acbc070cca64e61789555278cae5f0a
Deric <dericleee@gmail.com>
2023-05-01 03:12:51 -0700

    trips index delete

--- frontend/src/components/TripIndex/TripIndexItem.js
+++ frontend/src/components/TripIndex/TripIndexItem.js
@@ -12,16 +12,18 @@ const TripIndexItem = ({trip, id}) => {
     }
 
     return (
-        <>
-            <li id='trip-info-box'>
+        <div id='trip-info-container'>
+            <div id='trip-info-photo-box'>
                 <img id='trip-info-photo' src="https://cdn.dribbble.com/users/226242/screenshots/18014512/airbnb_icon.png" alt="photo" />
-                <p>{trip.listing.city}</p>
-                <p>Hosted by {trip.host.firstName}</p>
-                <p>{trip.startDate}</p>
-                <p>{trip.closingDate}</p>
+            </div>
+            <div id='trip-info-box'>
+                <p className='bold-header'>{trip.listing.city}</p>
+                <p id='trip-info'>Hosted by {trip.host.firstName}</p>
+                <p id='trip-info'>{trip.startDate}</p>
+                <p id='trip-info'>{trip.closingDate}</p>
                 <button onClick={handleClick}>Cancel Trip</button>
-            </li>
-        </>
+            </div>
+        </div>
     )
 }
 
--- frontend/src/components/TripIndex/index.js
+++ frontend/src/components/TripIndex/index.js
@@ -4,31 +4,45 @@ import { getTrips, fetchTrips } from "../../store/trip"
 import { useEffect } from "react"
 import TripIndexItem from "./TripIndexItem"
 import './TripIndex.css'
+import { NavLink } from "react-router-dom"
+import { Link } from "react-router-dom"
 
 const TripIndex = () => {
     const dispatch = useDispatch()
-    const sessioonUser = useSelector(state => state.session.user)
+    const sessionUser = useSelector(state => state.session.user)
     const trips = useSelector(getTrips)
-    const filterTrips = trips.filter(trip => trip.userId === sessioonUser.id)
-
-    console.log(filterTrips)
+    const filterTrips = trips.filter(trip => trip.userId === sessionUser.id)
     
+
     useEffect(()=>{
         dispatch(fetchTrips())
     }, [dispatch])
 
     return (
-        <div>
-            <ul id='trips-box-container'>
-                {
-                    filterTrips.map(trip => <TripIndexItem
-                        trip={trip}
-                        key={trip.id}
-                    />)
-                }
-            </ul>
-            <p>Can't find your reservation here?</p>
-            <p>Visit the Help Center</p>
+        <div className='new-page-render' id='trips-index-page'>
+            <div id='trips-index-header'>
+                <p>Trips</p>
+            </div>
+            <div id='upcoming-trips-box'>
+                <div id='upcoming-trips-banner'>
+                    <p className='trips-sub-header'>Upcoming Trips</p>
+                </div>
+                <div id='trips-info-box-container'>
+                    {
+                        filterTrips.map(trip => <TripIndexItem
+                            trip={trip}
+                            key={trip.id}
+                        />)
+                    }
+                </div>
+            </div>
+            <div id='past-trips-box'>
+                <p className='trips-sub-header'>Where you've been</p>
+            </div>
+            <div id='trips-index-footer-box'>
+                <p>Can't find your reservation here?</p>
+                <Link>Visit the Help Center</Link>
+            </div>
         </div>
     )
 }
